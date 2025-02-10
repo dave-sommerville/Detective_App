2ac5c705971ef702e3cc8e75a898959a40eb740e
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-02-10 09:22:33 -0600

    Save point

--- Models/Suspect.cs
+++ Models/Suspect.cs
@@ -7,7 +7,7 @@
         public string SuspectLastName { get; set; }
         public string SuspectFullName => $"{SuspectFirstName } + {SuspectLastName}";
         public string KnownAliases  { get; set; }
-        public string Mugshot {  get; set; } // Unsure of how to use 
+        public string Mugshot { get; set; } // Unsure of how to use 
         public string PhysicalDescription { get; set; }
         public string Motive { get; set; }
         public string Alibi { get; set; }
